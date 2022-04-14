c5fdfd62d5b9449c6d8a8feed319cce8b26dbded
aashi-s <shah.aashi28@gmail.com>
2022-04-13 23:42:34 -0400

    test commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.scss';
+import Component from './component.jsx'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit
+        </p>
+        <Component/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
